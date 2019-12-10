d9480b1de3600e5098e2e3d0a375bed934ad6d72
aurelija <a.drobelyte@gmail.com>
2019-12-10 16:55:50 +0200

    new spring boot project created

---
+++ src/main/java/it/akademija/parduotuve/ParduotuveApplication.java
@@ -0,0 +1,13 @@
+package it.akademija.parduotuve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParduotuveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParduotuveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/akademija/parduotuve/ParduotuveApplicationTests.java
@@ -0,0 +1,13 @@
+package it.akademija.parduotuve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParduotuveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
